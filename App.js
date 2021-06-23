182404633471f9814f97e8757cc09a27b1331126
Rezadi Falah <rezadifalah@gmail.com>
2021-06-23 08:16:24 +0700

    new register

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
-import firebase from 'firebase';
+import * as firebase from 'firebase';
 
+// Initialize Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyACR3outNtVe0b9pFjsBUav-QsA8MXQtN0",
+  authDomain: "myconstruction-5d8ca.firebaseapp.com",
+  projectId: "myconstruction-5d8ca",
+  storageBucket: "myconstruction-5d8ca.appspot.com",
+  messagingSenderId: "541065090179",
+  appId: "1:541065090179:web:debce1a2300ff40e64f7b6",
+  measurementId: "G-XYHGXP7CZW"
+};
 
+if(firebase.apps.length === 0){
+  firebase.initializeApp(firebaseConfig);
+}
 
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './src/components/Navigation/Navigation';
@@ -12,20 +25,7 @@ import Navigation from './src/components/Navigation/Navigation';
 export default function App() {
 
   
-  // Initialize Firebase
-  var firebaseConfig = {
-    apiKey: "AIzaSyACR3outNtVe0b9pFjsBUav-QsA8MXQtN0",
-    authDomain: "myconstruction-5d8ca.firebaseapp.com",
-    projectId: "myconstruction-5d8ca",
-    storageBucket: "myconstruction-5d8ca.appspot.com",
-    messagingSenderId: "541065090179",
-    appId: "1:541065090179:web:debce1a2300ff40e64f7b6",
-    measurementId: "G-XYHGXP7CZW"
-  };
   
-  if(firebase.apps.length === 0){
-    firebase.initializeApp(firebaseConfig);
-  }
   
   //firebase.analytics();
 
--- src/screens/Register/RegisterScreen.js
+++ src/screens/Register/RegisterScreen.js
@@ -1,41 +1,22 @@
-import React, {useState} from 'react';
+import React, { Component } from 'react';
 import {View, Text, Button, StyleSheet, TextInput} from 'react-native';
-import { Formik } from 'formik';
-import * as yup from 'yup';
-import firebase from 'firebase';
+import firebase from 'firebase'
 
-function RegisterScreen() {
+export class RegisterScreen extends Component {
+  constructor(props){
+    super(props);
 
-    const [form, setForm] = useState({
-      username: "",
-      email: "",
-      password: "",
-      cPassword: ""
-    })
-    
-    const loginValidationSchema = yup.object().shape({   
+    this.state = {
+      username = '',
+      email = '',
+      password = ''
+    }
 
-      username: yup
-        .string()
-        .required('Username is Required'),
-      email: yup
-        .string()
-        .email("Please enter valid email")
-        .required('Email Address is Required'),
-      password: yup
-        .string()
-        .min(8, ({ min }) => `Password must be at least ${min} characters`)
-        .required('Password is required'),
-      cPassword: yup
-        .string()
-        .min(8, ({ min }) => `Password must be at least ${min} characters`)
-        .required('Password is required'),
-    })
+    this.register = this.register.bind(this);
+  }
 
-    
-
-    const handleSubmit = () => {
-      const {email, password} = form;
+  register() {
+      const { username, email, password } = this.state;
       firebase.auth().createUserWithEmailAndPassword(email, password)
       .then((result) => {
         console.log(result)
@@ -43,106 +24,31 @@ function RegisterScreen() {
       .catch((err) => {
         console.log(err)
       })
-    }
+  }
 
-
+  render() {
     return (
-        <View style={styles.container}>
-        <Formik
-          enableReinitialize
-          validationSchema={loginValidationSchema}
-          initialValues={form}
-          onSubmit={(e) => {
-            handleSubmit();
-        }}
-        >
-           {({ handleChange, handleBlur, handleSubmit, values, errors, touched, isValid }) => (
-              <>
-                <TextInput
-                  name="username"
-                  placeholder="Username"
-                  style={styles.input}
-                  onChange={(e) => setForm(e.nativeEvent.text)}
-                  value={form.username}
-                />
-                <TextInput
-                  name="email"
-                  placeholder="Email Address"
-                  style={styles.input}
-                  onChangeText={handleChange('email')}
-                  onBlur={handleBlur('email')}
-                  value={form.email}
-                  
-                  keyboardType="email-address"
-                />
-                <TextInput
-                  name="password"
-                  placeholder="Password"
-                  style={styles.input}
-                  onChangeText={handleChange('password')}
-                  onBlur={handleBlur('password')}
-                  value={form.password}
-                  secureTextEntry
-                />
-                <TextInput
-                  name="cPassword"
-                  placeholder="Confirm Password"
-                  style={styles.input}
-                  onChangeText={handleChange('cPassword')}
-                  onBlur={handleBlur('cPassword')}
-                  value={form.cPassword}
-                  secureTextEntry
-                />
-                <Button onPress={handleSubmit} title="Submit" />
-              </>
-            )}
-        </Formik>
-        </View> 
+      <View>
+        <TextInput
+          placeholder="username"
+          onChangeText={(username) => this.setState({username})}
+        />
+        <TextInput
+          placeholder="email"
+          onChangeText={(email) => this.setState({email})}
+        />
+        <TextInput
+          placeholder="password"
+          onChangeText={(password) => this.setState({password})}
+          secureTextEntry
+        />
+        <Button
+          onPress={() => this.register()}
+          title="Register"
+        />
+      </View>
     )
+  }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#435C76'
-    },
-    btn: {
-      color: 'gray',
-      backgroundColor: 'ghostwhite',
-      width: 200,
-      fontSize: 16,
-      marginBottom : 10,
-      padding: 10,
-      textAlign: 'center',
-      borderColor: '#2076D1',
-      borderWidth: 1,
-      borderStyle: 'solid',
-      borderRadius: 10
-    },
-    opacity: {
-        color: 'red'
-    },
-    image: {
-        width: 100,
-        height: 100,
-        marginTop: 0,
-        marginBottom: 50
-    },
-    input: {
-        color: 'gray',
-        backgroundColor: 'ghostwhite',
-        width: 250,
-        fontSize: 14,
-        marginBottom : 10,
-        padding: 5,
-        textAlign: 'left',
-        borderColor: '#2076D1',
-        borderWidth: 1,
-        borderStyle: 'solid',
-        borderRadius: 10
-    }
-  });
 
 export default RegisterScreen
